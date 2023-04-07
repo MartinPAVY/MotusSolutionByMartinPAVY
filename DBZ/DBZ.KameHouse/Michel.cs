3a32748f7239b573f889a81caa2a3a699152c4db
MartinPAVY <113358964+MartinPAVY@users.noreply.github.com>
2023-04-07 17:29:39 +0200

    last push

--- DBZ/DBZ.KameHouse/Michel.cs
+++ DBZ/DBZ.KameHouse/Michel.cs
@@ -14,28 +14,46 @@ public class Michel : IGuerrier
 
     private bool _jeSuisCharge;
     private bool _adversaireCharge;
+    private int _attack = 0;
+    private int _round = 0;
 
     public ActionDeCombat ChoixProchaineAction(ActionDeCombat dernierActionAdversaire)
     {
-        if (dernierActionAdversaire == ActionDeCombat.ChargeKameHameHa)
+        /*if (_round >=1000)
+        {
+            return ("Combat infini")
+        }*/
+        if (dernierActionAdversaire == ActionDeCombat.ChargeKameHameHa )
         {
             this._adversaireCharge = true;
             return ActionDeCombat.Parade;
+            this._round += 1;
         } else if (dernierActionAdversaire == ActionDeCombat.KameHameHa && this._jeSuisCharge)
         {
             this._adversaireCharge = false;
             this._jeSuisCharge = false;
+            this._round += 1;
+            this._attack += 1;
             return ActionDeCombat.KameHameHa;
         }
         else if (dernierActionAdversaire == ActionDeCombat.KameHameHa && !this._jeSuisCharge)
         {
             this._adversaireCharge = false;
             this._jeSuisCharge = true;
+            this._round += 1;
+            return ActionDeCombat.ChargeKameHameHa;
+        }
+        else if (dernierActionAdversaire == ActionDeCombat.Parade && !this._jeSuisCharge)
+        {
+            this._adversaireCharge = false;
+            this._jeSuisCharge = true;
+            this._round += 1;
             return ActionDeCombat.ChargeKameHameHa;
         }
         else
         {
             this._jeSuisCharge = true;
+            this._round += 1;
             return ActionDeCombat.ChargeKameHameHa;
         }
     }
